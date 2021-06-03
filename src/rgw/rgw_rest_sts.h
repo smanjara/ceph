153eecc923c74b169d54ff0c9b32104ebbe7cd07
Pritha Srivastava <prsrivas@redhat.com>
2021-06-03 15:33:03 +0530

    rgw/sts: code to add role tags as aws:PrincipalTags and iam:ResourceTags.
    fixes: rhbz#2006174
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>
    (cherry picked from commit 58f1bb65bec5c7af2769732ca8748f2943242fb2)
    (cherry picked from commit 68db453c6f31f52cb13148163369983987de800b)
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_rest_sts.h
+++ src/rgw/rgw_rest_sts.h
@@ -37,7 +37,11 @@ class WebTokenEngine : public rgw::auth::Engine {
   std::string get_role_tenant(const string& role_arn) const;
 
   std::string get_jwks_url(const string& iss, const DoutPrefixProvider *dpp,optional_yield y) const;
+  
+  std::string get_role_name(const string& role_arn) const;
 
+  std::string get_cert_url(const std::string& iss, const DoutPrefixProvider *dpp,optional_yield y) const;
+  
   std::tuple<boost::optional<WebTokenEngine::token_t>, boost::optional<WebTokenEngine::principal_tags_t>>
   get_from_jwt(const DoutPrefixProvider* dpp, const std::string& token, const req_state* const s, optional_yield y) const;
 
@@ -101,9 +105,10 @@ class DefaultStrategy : public rgw::auth::Strategy,
                                     const string& role_session,
                                     const string& role_tenant,
                                     const std::unordered_multimap<string, string>& token,
+                                    boost::optional<std::multimap<std::string, std::string>> role_tags,
                                     boost::optional<std::set<std::pair<std::string, std::string>>> principal_tags) const override {
     auto apl = rgw::auth::add_sysreq(cct, ctl, s,
-      rgw::auth::WebIdentityApplier(cct, ctl, role_session, role_tenant, token, principal_tags));
+      rgw::auth::WebIdentityApplier(cct, ctl, role_session, role_tenant, token, role_tags, principal_tags));
     return aplptr_t(new decltype(apl)(std::move(apl)));
   }
 
