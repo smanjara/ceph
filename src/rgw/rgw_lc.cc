89b7b24a1735b58febf06172cc0131d868e28616
Mark Kogan <mkogan@redhat.com>
2022-03-15 15:06:56 +0200

    rgwlc: fix segfault resharding during lc
    Fixes: https://tracker.ceph.com/issues/54363
    Resolves: rhbz#2064327
    
    Signed-off-by: Mark Kogan <mkogan@redhat.com>
    (cherry picked from commit 7d2e72a9d0451e36141282d6456a4c23d753b592)
    (cherry picked from commit a0a60154762b80142e5b9d9c1c7edb75c7f68bc2)
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -472,7 +472,7 @@ public:
         if (ret < 0) {
           ldout(store->ctx(), 0) << "ERROR: list_op returned ret=" << ret
 				 << dendl;
-          return ret;
+          return false;
         }
       }
       delay();
