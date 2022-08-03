scantist-ossops-m2/ceph-ci

ceph-volume: add raw support for db/wal for list and activate
Currently 'prepare' doesn't support db/wal, but we want it in list and
activate because 'ceph-volume activate ...' tries raw before lvm.

Note that I'm not sure we really want to accept --block.db and --block.wal
here at all.

Signed-off-by: Sage Weil <sage@newdream.net>

src/ceph-volume/ceph_volume/devices/raw/activate.py
@@ -35,21 +35,27 @@ def activate_bluestore(meta, tmpfs, systemd, block_wal=None, block_db=None):
     system.chown(osd_path)
     prime_command = [
         'ceph-bluestore-tool',
-        'prime-osd-dir', '--dev', meta['device'],
+        'prime-osd-dir',
         '--path', osd_path,
-        '--no-mon-config']
+        '--no-mon-config',
+        '--dev', meta['device'],
+    ]
     process.run(prime_command)
 
     # always re-do the symlink regardless if it exists, so that the block,
     # block.wal, and block.db devices that may have changed can be mapped
     # correctly every time
-    prepare_utils.link_block( meta['device'], osd_id)
-
-    if block_wal:
-        prepare_utils.link_wal(block_wal, osd_id, osd_uuid)
+    prepare_utils.link_block(meta['device'], osd_id)
 
     if block_db:
         prepare_utils.link_db(block_db, osd_id, osd_uuid)
+    elif 'device_db' in meta:
+        prepare_utils.link_wal(meta['device_db'], osd_id, osd_uuid)
+
+    if block_wal:
+        prepare_utils.link_wal(block_wal, osd_id, osd_uuid)
+    elif 'device_wal' in meta:
+        prepare_utils.link_wal(meta['device_wal'], osd_id, osd_uuid)
 
     system.chown(osd_path)
     terminal.success("ceph-volume raw activate successful for osd ID: %s" % osd_id)

smanjara/ceph

Resolves: rhbz#2093017
Resolves: rhbz#2093022
Fixes: 3d7ceec684b0ac5b83fae4c397b134236fac485e
(cherry picked from commit f0a0c70d5c8c150abd0590ea23be83c7e53f9a10)
(cherry picked from commit 8242ec16e5dd0bdabbb4bb6f39dbc0fa1e515713)

src/ceph-volume/ceph_volume/devices/raw/activate.py
@@ -35,21 +35,27 @@ def activate_bluestore(meta, tmpfs, systemd, block_wal=None, block_db=None):
     system.chown(osd_path)
     prime_command = [
         'ceph-bluestore-tool',
-        'prime-osd-dir', '--dev', meta['device'],
+        'prime-osd-dir',
         '--path', osd_path,
-        '--no-mon-config']
+        '--no-mon-config',
+        '--dev', meta['device'],
+    ]
     process.run(prime_command)
 
     # always re-do the symlink regardless if it exists, so that the block,
     # block.wal, and block.db devices that may have changed can be mapped
     # correctly every time
-    prepare_utils.link_block( meta['device'], osd_id)
-
-    if block_wal:
-        prepare_utils.link_wal(block_wal, osd_id, osd_uuid)
+    prepare_utils.link_block(meta['device'], osd_id)
 
     if block_db:
         prepare_utils.link_db(block_db, osd_id, osd_uuid)
+    elif 'device_db' in meta:
+        prepare_utils.link_db(meta['device_db'], osd_id, osd_uuid)
+
+    if block_wal:
+        prepare_utils.link_wal(block_wal, osd_id, osd_uuid)
+    elif 'device_wal' in meta:
+        prepare_utils.link_wal(meta['device_wal'], osd_id, osd_uuid)
 
     system.chown(osd_path)
     terminal.success("ceph-volume raw activate successful for osd ID: %s" % osd_id)