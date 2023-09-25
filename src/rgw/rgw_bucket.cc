59f389af8d59e02bb4a533a8bee48d2cd3a9ad9f
Cory Snyder <csnyder@1111systems.com>
2023-09-25 10:06:41 +0000

    rgw: fix output formatting of bucket index check admin api
    The bucket index check admin API was previously returning invalid
    JSON.
    
    Resolves: rhbz#2257373
    
    Signed-off-by: Cory Snyder <csnyder@1111systems.com>
    Signed-off-by: matt benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_bucket.cc
+++ src/rgw/rgw_bucket.cc
@@ -981,6 +981,8 @@ int RGWBucketAdminOp::check_index(rgw::sal::Store* store, RGWBucketAdminOpState&
   Formatter *formatter = flusher.get_formatter();
   flusher.start(0);
 
+  formatter->open_object_section("bucket_check");
+
   ret = bucket.check_bad_index_multipart(op_state, flusher, dpp);
   if (ret < 0)
     return ret;
@@ -996,6 +998,7 @@ int RGWBucketAdminOp::check_index(rgw::sal::Store* store, RGWBucketAdminOpState&
     return ret;
 
   dump_index_check(existing_stats, calculated_stats, formatter);
+  formatter->close_section();
   flusher.flush();
 
   return 0;
